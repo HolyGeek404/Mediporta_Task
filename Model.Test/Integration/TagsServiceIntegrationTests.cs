cd960dce0e225bdf408292e922c907720e0049ac
HolyGeek404 <wmatkowski404@proton.me>
2025-09-15 17:57:20 +0200

    Added integration test

---
+++ Model.Test/Integration/TagsServiceIntegrationTests.cs
@@ -0,0 +1,83 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using NUnit.Framework;
+using Model.DataAccess;
+using Model.DataAccess.Context;
+using Model.DataAccess.Entities;
+using Model.DataAccess.Interfaces;
+using Model.Services;
+using Model.Services.Interfaces;
+using Model.Features.Queries.GetTags;
+
+[TestFixture]
+public class TagsServiceIntegrationTests
+{
+    private ServiceProvider _serviceProvider = default!;
+    private ITagsService _tagsService = default!;
+    private TagsContext _context = default!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        var services = new ServiceCollection();
+
+        services.AddDbContext<TagsContext>(options =>
+            options.UseSqlite("Data Source=:memory:"));
+
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection()
+            .Build();
+        services.AddSingleton<IConfiguration>(configuration);
+
+        var mockFactory = new Mock<IHttpClientFactory>();
+        services.AddSingleton(mockFactory.Object);
+
+        var mockBuilder = new Mock<IRequestMessageBuilder>();
+        services.AddSingleton(mockBuilder.Object);
+
+        services.AddTransient<ITagsDao, TagsDao>();
+        services.AddTransient<ITagsService, TagsService>();
+
+        _serviceProvider = services.BuildServiceProvider();
+
+        _context = _serviceProvider.GetRequiredService<TagsContext>();
+        _context.Database.OpenConnection(); 
+        _context.Database.EnsureCreated();  
+
+        _tagsService = _serviceProvider.GetRequiredService<ITagsService>();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _context.Database.CloseConnection();
+        _context.Dispose();
+        _serviceProvider.Dispose();
+    }
+
+    [Test]
+    public async Task GetTags_ShouldReturnTags_WhenDatabaseHasData()
+    {
+        _context.Tags.Add(new Tag { Name = "csharp", Count = 100 });
+        _context.Tags.Add(new Tag { Name = "dotnet", Count = 50 });
+        await _context.SaveChangesAsync();
+
+        var query = new GetTagsQuery
+        {
+            Page = 1,
+            PageSize = 10,
+            Sort = "name",
+            Order = "asc"
+        };
+
+        var tags = await _tagsService.GetTags(query, CancellationToken.None);
+
+        Assert.IsNotNull(tags);
+        Assert.AreEqual(2, tags.Count);
+        Assert.AreEqual("csharp", tags[0].Name);
+    }
+}
